b058ef4b47755b7cd0a9f522f5a9ba52aa9f864f
anwarservice <35414342+anwarservice@users.noreply.github.com>
2023-01-31 14:58:29 +0700

    Create turtle1.py

---
+++ บทที่ 5 turtle Graphic/turtle1.py
@@ -0,0 +1,23 @@
+import turtle
+import time
+def draw_circle(turtle, color, size, x, y):
+    turtle.penup()
+    turtle.color(color)
+    turtle.fillcolor(color)
+    turtle.goto(x,y)
+    turtle.begin_fill()
+    turtle.circle(size)
+    turtle.end_fill()
+    turtle.pendown()
+
+tommy = turtle.Turtle()
+tommy.shape("turtle")
+tommy.speed(1)
+
+draw_circle(tommy, "green", 50, 25, 0)
+draw_circle(tommy, "blue", 50, 0, 0)
+draw_circle(tommy, "yellow", 50, -25, 0)
+
+
+print("Welcome to Banpami Python Tutorials")
+time.sleep(10)
